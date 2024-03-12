79be7c7578a40bc94d3a0b3ec6324892e16c9ee8
Aryo brokolly <98819851+aryobrokolly@users.noreply.github.com>
2024-03-13 00:43:30 +0700

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from huawei_lte_api.Connection import Connection
+from huawei_lte_api.Client import Client
+
+parser = ArgumentParser()
+parser.add_argument('url', type=str)
+parser.add_argument('--username', type=str)
+parser.add_argument('--password', type=str)
+args = parser.parse_args()
+
+with Connection(args.url, username=args.username, password=args.password) as connection:
+    client = Client(connection)
+    wan_ip_address = client.device.information()['WanIPAddress']
+    print(f'IP: {wan_ip_address}')
